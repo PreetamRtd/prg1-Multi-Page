a2905c2f21f79b56413679596925b86626138ca8
preetamrtd <preetam.rtd@gmail.com>
2024-02-06 23:21:40 +0530

    bgc changed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,11 @@
-const pagenum = document.getElementById("pagenum");
 const postContainer = document.getElementById("post-container");
 const postInput = document.getElementById("post-input");
-let count = 0;
-function btnclick(pgn) {
-    if (pgn == 1) {
-        pagenum.innerHTML = "1";
-    } else if (pgn == 2) {
-        pagenum.innerHTML = "2";
-    }
-}
+
 
 function postclick() {
     const HTMLString = `
     <div class="post">
-    <div class="user-name">-9611451585</div>
+    <div class="user-name">me</div>
     <br>
     <div class="post-content">
         ${postInput.value}
